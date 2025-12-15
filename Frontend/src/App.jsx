667120e3f06c734c5d615756321a89974287e7bc
Satvik Gupta <satvik24gupta@gmail.com>
2025-12-15 22:44:20 +0530

    initial setup code for frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+// import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-red-500'>Welcome to my blog website</h1>
+    </>
+  )
+}
+
+export default App
